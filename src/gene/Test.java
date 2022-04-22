931770a6bf1f6b0e80046637705ec4eaf90e52a1
Giri prasad <ainugiri@gmail.com>
2022-04-22 10:42:50 +0530

    202204221042

---
+++ src/gene/Test.java
@@ -0,0 +1,30 @@
+package gene;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// collection
+		// array simi
+		// a[0]
+//		ArrayList list = new ArrayList();
+//		list.add("10");
+//		list.add("Giri ")
+//		list.add(12);
+//		
+		// 5.0
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(15);
+		list.add(12);
+//		list.add("55a5sd");  type safety 
+		
+		// Generics 
+//		List l1 = new ArrayList();
+//		l1.add("hello");
+//		l1.add("World");
+//		String s1 = (String)l1.get(0);
+		System.out.println("Value "+list.get(0));
+	}
+}
