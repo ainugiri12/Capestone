fd974bad15f6ee120fda2eec6a2715492a41f868
Giri prasad <ainugiri@gmail.com>
2022-04-28 10:26:38 +0530

    20220428

---
+++ src/mvc/Student.java
@@ -0,0 +1,33 @@
+package mvc;
+
+public class Student {
+	int stID;
+	String fName;
+	String lName;
+	int mobNo;
+	public int getStID() {
+		return stID;
+	}
+	public void setStID(int stID) {
+		this.stID = stID;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public int getMobNo() {
+		return mobNo;
+	}
+	public void setMobNo(int mobNo) {
+		this.mobNo = mobNo;
+	}
+	
+}
---
+++ src/mvc/Test.java
@@ -0,0 +1,9 @@
+package mvc;
+
+public class Test {
+public static void main(String[] args) {
+	Student s1 = new Student();
+	s1.setfName("Giri");
+	System.out.println(s1.getfName());
+}
+}
